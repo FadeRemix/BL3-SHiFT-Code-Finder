3ee2bff4a65537f4c588ebe774f6a51c0922734a
Fade <64497456+FadeRemix@users.noreply.github.com>
2024-03-25 07:35:47 -0400

    Add files via upload

---
+++ CodeFinder2.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://mentalmars.com/game-news/borderlands-3-golden-keys/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+name_results = soup.find("tbody")
+for child in name_results.children:
+    print(child.get_text(" | ",strip = True))
+    
+input("Press Enter to exit...")
